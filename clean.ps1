9174722ea125666d63194026bb8c94213004b006
Santi PCEscritorio <santimussi1959@gmail.com>
2026-06-09 20:50:57 -0300

    Clean

--- clean.ps1
+++
@@ -1,5 +0,0 @@
-Get-ChildItem -Path src -Recurse -Include *.tsx,*.ts,*.css | ForEach-Object {
-    $content = Get-Content $_.FullName -Raw -Encoding UTF8
-    $content = $content -replace '[─═]+', ''
-    Set-Content $_.FullName -Value $content -Encoding UTF8
-}
